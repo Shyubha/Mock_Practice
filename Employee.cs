fc45a68bae18f9a0bac919481457c9a90b1576cc
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-19 19:56:41 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Employee
+    {
+        public int Id
+        {
+            get { return id;}
+            set { id = value;}
+        }
+        public string Name
+        {
+            get { return name;}
+            set { name = value;}
+        }
+        public int Salary
+        {
+            get{ return salary;}
+            set { salary = value;}
+        }
+        public string city
+        {
+            get { return city;}
+
+        }
+
+        int id;
+        string name;
+        int salary;
+        String City;
+        public Employee()
+        {
+            
+        }
+        public Employee(int id,string name,int Salary,string city)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = Salary;
+            this.City = city;
+
+            
+        }
+        public override string ToString()
+        {
+            return id + " " + name + " " + salary + " " +city;  
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+            list.Add  (new Employee(1,"Shubhankar",60000,"PUNE"));
+            list.Add(new Employee(2, "Nilesh", 50000,"Solapur"));
+            list.Add(new Employee(3, "Akash", 10000,"Nagpur"));
+            list.Add(new Employee(5, "Amit", 40000, "PUNE"));
+            list.Add(new Employee(6, "Akshay", 50000, "Pune"));
+            list.Add(new Employee(7, "Parag", 10000, "Pune"));
+
+
+            list.Insert(0, new Employee(22, "Amit", 40000,"Mumbai"));
+
+            var x = from e in list
+                    where e.Salary < 15000
+                     select e;
+            Console.WriteLine(x);
+          
+
+            var y = from e in list
+                    where e.Name.StartsWith("a")
+                    select e;
+            foreach (Employee e in y)
+            {
+                Console.WriteLine(e);
+            }
+
+            var z = from e in list
+                    orderby e.Salary descending
+                    select e;
+            foreach (Employee e in z)
+            {
+                Console.WriteLine(e);
+            }
+            var w = from e in list
+                    where e.city.Contains("Pune")
+                    orderby e.Salary ascending
+                    select e;
+            foreach (Employee e in w)
+            {
+                Console.WriteLine(e);
+            }
+
+            var V = from e in list
+                    where e.Salary >= 25000 && e.Salary <= 40000
+                    orderby e.Name descending
+                    select e;
+            foreach (Employee e in V)
+            {
+                Console.WriteLine(e);
+            }
+
+            foreach (Employee e in list)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("____");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
